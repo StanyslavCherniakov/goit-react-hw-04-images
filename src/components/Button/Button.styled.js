f93ff196ad3930c087941595428db4254850a5bf
StanyslavCherniakov <stas.chernykov@gmail.com>
2022-12-22 01:47:20 +0200

    fix loadmore btn

--- src/components/Button/Button.styled.js
+++ src/components/Button/Button.styled.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
 export const LoadBtn = styled.button`
+  margin-left: auto;
+  margin-right: auto;
   padding: 8px 16px;
   border-radius: 2px;
   background-color: #3f51b5;
@@ -16,7 +18,7 @@ export const LoadBtn = styled.button`
   line-height: 24px;
   font-style: normal;
   font-weight: 500;
-  min-width: 180px;
+  width: 180px;
   box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
   0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
 
