8a409867e1b6c34e89f3d810a64e385ac8c7466a
StanyslavCherniakov <stas.chernykov@gmail.com>
2022-12-22 08:35:31 +0200

    add loader styles

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { RotatingLines } from 'react-loader-spinner';
+import { Wrapper } from './Loader.styled';
 
 export const Loader = () => {
   return (
-    <RotatingLines
-      strokeColor='grey'
-      strokeWidth='5'
-      animationDuration='0.75'
-      width='96'
-      visible={true}
-    />
+    <Wrapper>
+      <RotatingLines
+        strokeColor='grey'
+        strokeWidth='5'
+        animationDuration='0.75'
+        width='96'
+        visible={true}
+      />
+    </Wrapper>
   );
 };
 
---
+++ src/components/Loader/Loader.styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  margin-left: auto;
+  margin-right: auto;
+`;
